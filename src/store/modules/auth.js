5448fa03aaf9750a12810b375a861ace767cf70f
kinako555 <dodohunter_3658@yahoo.co.jp>
2020-04-09 02:40:08 +0900

    a

---
+++ src/store/modules/auth.js
@@ -0,0 +1,41 @@
+export default {
+    namespaced: true,
+    state: {
+        tenant: '',
+        userId: '',
+        token: ''
+    },
+    mutations: {
+        create(state, data) {
+            state.tenant = ''
+            state.userId = data.user_id
+            state.token = data.token
+        },
+        destroy(state) {
+            state.tenant = ''
+            state.userId = ''
+            state.token = ''
+        }
+    },
+    actions: {
+        // 他moduleをコールcoalする際、第3引数で{root: true}にしないとエラーになる
+        create({ commit, dispatch }, data) {
+            dispatch(
+                'http/post',
+                { url: '/auth', data, error: 'message.unauthorized' },
+                { root: true }
+            ).then(res => commit('create', res.data))
+             .catch(err => err)
+        },
+        destroy({ commit, dispatch }, data) {
+            dispatch(
+                'http/delete',
+                { url: '/auth', data },
+                { root: true }
+            ).then(res => commit('create', res.data))
+             .catch(err => err)
+             .finally(res => commit('destroy'))
+        }
+    }
+
+}
